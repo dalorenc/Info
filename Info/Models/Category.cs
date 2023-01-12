739b22a566cd744808575aa54cf10426fcda35e4
dlorenc <lorencdaniel0@gmail.com>
2023-01-13 00:47:35 +0100

    relacjePomiedzyTabelamiPoczatek

---
+++ Info/Models/Category.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Info.Models
+{
+    public class Category
+    {
+        [Key] //zbędne oznaczenie, gdyż w nazwie pola występuje ciąg Id
+        [Display(Name = "Identyfikator kategorii:")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę kategorii.")]
+        [Display(Name = "Nazwa kategorii: ")]
+        [MaxLength(50, ErrorMessage = "Nazwa kategorii nie może być dłuższy niż 50 znaków.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać opis kategorii.")]
+        [Display(Name = "Opis kategorii: ")]
+        [MaxLength(255, ErrorMessage = "Opis kategorii nie może być dłuższy niż 255 znaków.")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę ikony.")]
+        [Display(Name = "Ikona kategorii: ")]
+        [MaxLength(30, ErrorMessage = "Opis ikony nie może być dłuższy niż 30 znaków.")]
+        public string? Icon { get; set; }
+
+        [Required]
+        [Display(Name = "Czy aktywna?")]
+        [DefaultValue(true)]
+        public bool Active { get; set; }
+
+        [Required]
+        [Display(Name = "Czy aktywna?")]
+        [DefaultValue(true)]
+        public bool Display { get; set; }
+    }
+}
---
+++ Info/Models/Opinion.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Info.Models
+{
+    public class Opinion
+    {
+        [Key]
+        [Display(Name = "Identyfikator:")]
+        public int OpinionId { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać treść komentarza.")]
+        [Display(Name = "Treść komentarza:")]
+        [DataType(DataType.MultilineText)]
+        public string? Comment { get; set; }
+
+        [Display(Name = "Data dodania:")]
+        [DataType(DataType.Date, ErrorMessage = "Niepoprawny format daty")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy HH:mm}")]
+        public System.DateTime AddedDate { get; set; }
+
+        [Display(Name = "Ocena tekstu:")]
+        public TypeOfGrade? Rating { get; set; }
+
+        [Required]
+        [Display(Name = "Komentowany tekst:")]
+        public int TextId { get; set; }
+
+        [Display(Name = "Autor komentarza:")]
+        public string? Id { get; set; }
+
+    }
+
+    public enum TypeOfGrade
+    {
+        super = 5,
+        dobry = 4,
+        przeciętny = 3,
+        słaby = 2,
+        nieprzydatny = 1,
+    }
+}
