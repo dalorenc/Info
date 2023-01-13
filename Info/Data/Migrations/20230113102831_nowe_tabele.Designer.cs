fc22f3a92db86b03eef188a6ac048af923a3fd46
dlorenc <lorencdaniel0@gmail.com>
2023-01-13 11:37:42 +0100

    migracjeCzyliBudowaTabel

--- Info/Data/Migrations/20230113102831_nowe_tabele.Designer.cs
+++ Info/Data/Migrations/20230113102831_nowe_tabele.Designer.cs
@@ -4,6 +4,7 @@ using Info.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Info.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230113102831_nowe_tabele")]
+    partial class nowe_tabele
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
