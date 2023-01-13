cb4c3075f95a574d9f052bdbeaaf44aa7b46675d
dlorenc <lorencdaniel0@gmail.com>
2023-01-13 11:12:57 +0100

    bazaDanych

--- Info/Data/ApplicationDbContext.cs
+++ Info/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Info.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Info.Data
@@ -8,6 +9,39 @@ namespace Info.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            public DbSet<Category>? Categories { get; set; }
+            public DbSet<Text>? Texts { get; set; }
+            public DbSet<Opinion>? Opinions { get; set; }
+            public DbSet<AppUser>? AppUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+            modelbuilder.Entity<Category>()
+            .HasMany(c => c.Texts)
+            .WithOne(t => t.Category);
+            modelbuilder.Entity<Text>()
+            .HasOne(t => t.Category)
+            .WithMany(c => c.Texts);
+            modelbuilder.Entity<Text>()
+            .HasMany(t => t.Opinions)
+            .WithOne(o => o.Text);
+            modelbuilder.Entity<Opinion>()
+            .HasOne(o => o.Text)
+            .WithMany(t => t.Opinions)
+            .OnDelete(DeleteBehavior.Restrict);
+            modelbuilder.Entity<AppUser>()
+            .HasMany(u => u.Texts)
+            .WithOne(t => t.User);
+            modelbuilder.Entity<Text>()
+            .HasOne(u => u.User)
+            .WithMany(u => u.Texts);
+            modelbuilder.Entity<AppUser>()
+            .HasMany(u => u.Opinions)
+            .WithOne(o => o.User);
+            modelbuilder.Entity<Opinion>()
+            .HasOne(o => o.User)
+            .WithMany(u => u.Opinions);
         }
+    }
     }
 }
