1f29dda1f1fe0119528962c78b75c657b68b237b
dlorenc <lorencdaniel0@gmail.com>
2023-01-13 19:12:31 +0100

    koniecStronicowania

--- Info/Controllers/TextsController.cs
+++ Info/Controllers/TextsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Info.Data;
 using Info.Models;
+using Info.Models.ViewModels;
 
 namespace Info.Controllers
 {
@@ -20,7 +21,30 @@ namespace Info.Controllers
         }
 
         // GET: Texts
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int PageNumber = 1)
+        {
+            TextsViewModel textsViewModel = new();
+            textsViewModel.TextsView = new TextsView();
+
+            textsViewModel.TextsView.TextCount = _context.Texts
+                .Where(t => t.Active == true)
+                .Count();
+
+            textsViewModel.TextsView.PageNumber = PageNumber;
+
+            textsViewModel.Texts = (IEnumerable<Text>?)await _context.Texts
+                .Include(t => t.Category)
+                .Include(t => t.User)
+                .Where(t => t.Active == true)
+                .OrderByDescending(t => t.AddedDate)
+                .Skip((PageNumber - 1) * textsViewModel.TextsView.PageSize)
+                .Take(textsViewModel.TextsView.PageSize)
+                .ToListAsync();
+           
+            return View(textsViewModel);
+        }
+        // GET: Texts
+        public async Task<IActionResult> List()
         {
             var applicationDbContext = _context.Texts.Include(t => t.Category).Include(t => t.User);
             return View(await applicationDbContext.ToListAsync());
---
+++ Info/Models/ViewModels/TextsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Info.Models.ViewModels
+{
+    public class TextsViewModel
+    {
+        public IEnumerable<Text>? Texts { get; set; }
+        public TextsView? TextsView { get; set; }
+
+    }
+}
